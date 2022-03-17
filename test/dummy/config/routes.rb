b659180535e005bf3b836bc761cda2a750adb734
Joel Azemar <joel.azemar@gmail.com>
2022-03-17 14:59:48 +0100

    Update Dummy App

--- test/dummy/config/routes.rb
+++ test/dummy/config/routes.rb
@@ -1,7 +1,6 @@
-# frozen_string_literal: true
-
 Rails.application.routes.draw do
-  resources :articles
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  root 'articles#index'
+  # Defines the root path route ("/")
+  # root "articles#index"
 end
